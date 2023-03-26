5098da33e5ad58e67ee84947c30d4ec8cb3d0a51
Yura Zabolotin <zabolotiny@gmail.com>
2023-03-26 11:52:19 +0200

    Initial Commit

---
+++ GoogleSignIn-Carthage/GoogleSignIn_Carthage.h
@@ -0,0 +1,18 @@
+//
+//  GoogleSignIn_Carthage.h
+//  GoogleSignIn-Carthage
+//
+//  Created by Yura Zabolotin on 26.03.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleSignIn_Carthage.
+FOUNDATION_EXPORT double GoogleSignIn_CarthageVersionNumber;
+
+//! Project version string for GoogleSignIn_Carthage.
+FOUNDATION_EXPORT const unsigned char GoogleSignIn_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleSignIn_Carthage/PublicHeader.h>
+
+
