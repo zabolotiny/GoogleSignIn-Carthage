19612dc2bb67c909ae1633dc4cbaf60428311756
Yura Zabolotin <zabolotiny@gmail.com>
2023-03-26 11:55:01 +0200

    GoogleSignIn carthage compatibly

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '13.0'
+inhibit_all_warnings!
+
+target 'GoogleSignIn-Carthage' do
+  use_frameworks!
+  pod 'GoogleSignIn', '~> 5.0.2'
+end
---
+++ Pods/Target Support Files/Pods-GoogleSignIn-Carthage/Pods-GoogleSignIn-Carthage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleSignIn_Carthage : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleSignIn_Carthage
+@end
